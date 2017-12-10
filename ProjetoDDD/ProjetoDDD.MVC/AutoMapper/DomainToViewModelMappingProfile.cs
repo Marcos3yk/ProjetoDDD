09da4258d5e48f96223ef27d431060131ba90e81
Marcos <marcos3yk@gmail.com>
2017-12-10 12:52:44 -0300

    Criação da View Clientes

--- ProjetoDDD/ProjetoDDD.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
+++ ProjetoDDD/ProjetoDDD.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using AutoMapper;
+using ProjetoDDD.Domain.Entities;
+using ProjetoDDD.MVC.ViewModels;
+
 
 namespace ProjetoDDD.MVC.AutoMapper
 {
-    public class DomainToViewModelMappingProfile
+    public class DomainToViewModelMappingProfile : Profile
     {
+        public override string ProfileName
+        {
+            get { return "ViewModelToDomainMappings"; }
+        }
+        protected override void Configure()
+        {
+            Mapper.CreateMap<ClienteViewModel, Cliente>();
+            Mapper.CreateMap<ProdutoViewModel, Produto>();
+        }
     }
 }
--- ProjetoDDD/ProjetoDDD.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ ProjetoDDD/ProjetoDDD.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using AutoMapper;
+using ProjetoDDD.Domain.Entities;
+using ProjetoDDD.MVC.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace ProjetoDDD.MVC.AutoMapper
 {
-    public class ViewModelToDomainMappingProfile
+    public class ViewModelToDomainMappingProfile : Profile
     {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+        protected override void Configure()
+        {
+            Mapper.CreateMap<ClienteViewModel, Cliente>();
+            Mapper.CreateMap<ProdutoViewModel, Produto>();
+        }
     }
 }
